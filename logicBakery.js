846aa2ead2c81f7a731d4fa864c5374c26d9867d
Orquidea <orquidea_238@hotmail.fr>
2019-10-22 12:07:59 +0200

    first commit

---
+++ logicBakery.js
@@ -0,0 +1,9 @@
+const farine = 1;
+const sucre = 1.5 * 2;
+const oeufs = 3 * 2;
+const lait = 2.5/5;
+const chocolat = 10*2;
+const decos = 5*3.5;
+
+const prix = farine + sucre + oeufs + lait + chocolat + decos;
+console.log(prix);
